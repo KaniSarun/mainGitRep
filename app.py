b60a75361a6e8b69eda27805d6e75d4de51b323c
KaniSarun <joshex360@hotmail.com>
2025-05-21 10:18:01 -0500

    Prueba de pagina

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("Mi primera publicacion")
 st.header("Introduccion")
 st.write("Esta es una prueba de streamlit")
+st.write("Esta es la segunda prueba pa actualizar la pagina")
